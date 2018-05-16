d3e48eb4c6ff05fb5bf0b28602227bef37a647db
aimms321 <aimms321@163.com>
2018-05-17 01:55:54 +0800

    0516

--- house-web/src/test/java/com/project/house/UserServiceTest.java
+++ house-web/src/test/java/com/project/house/UserServiceTest.java
@@ -1,25 +1,28 @@
-package com.project.house.biz.service;
+package com.project.house;
 
+import com.project.house.biz.service.UserService;
 import com.project.house.common.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * Created by user on 2018-05-16.
- */
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = com.project.house.web.HouseApplication.class)
 public class UserServiceTest {
     @Autowired
     private UserService userService;
 
     @Test
     public void testAuth() {
-        User user = userService.auth("houseproject@163.com", "11111");
-        assert user != null;
+//        User user = userService.auth("houseproject@163.com", "222222");
+//        assert user != null;
+        List<User> users = userService.getUsers();
+        assert !users.isEmpty();
     }
 }
